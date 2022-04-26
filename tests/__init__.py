90721fdd38ae6fe20ef3d6a6adf984c2f09c73e4
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:26:18 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part sale_remaining_stock module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_sale_remaining_stock import suite
-
-__all__ = ['suite']
